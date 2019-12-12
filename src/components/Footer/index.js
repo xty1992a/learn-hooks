9ed705de4d2bd27ef2147d75b18fd4879afad31f
谢腾远 <redbuck@126.com>
2019-12-12 18:41:35 +0800

    添加了图标

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import "./index.less";
-import {Link} from "react-router-dom";
+import {Link, useLocation, useRouteMatch} from "react-router-dom";
+import menu_my from 'assets/icons/menu_my.svg';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 
 export default function Footer() {
+  const query = useQuery();
+  let {path, url} = useRouteMatch();
+  console.log(url, path, query.get('id'));
   return (
     <div className="footer">
       <ul className="footer-menus">
-        <li className="footer-menu">
-          <Link to="/about">about</Link>
+        <li className="footer-menu touch-action">
+          <Link to="/about?id=asdf">
+            <img src={menu_my} alt=""/>
+            <p>About</p>
+          </Link>
         </li>
-        <li className="footer-menu">
-          <Link to="/home">Home</Link>
+        <li className="footer-menu touch-action">
+          <Link to="/home">
+            <img src={menu_my} alt=""/>
+            <p>Home</p>
+          </Link>
         </li>
       </ul>
     </div>
